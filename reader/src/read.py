637ecf0bc6df42bede68b3ab8c6fde9f057d754d
Matt Mazzola <mattmazzola@live.com>
2023-01-02 21:16:04 -0800

    Print unitdone events

--- reader/src/read.py
+++ reader/src/read.py
@@ -2,30 +2,35 @@ import sc2reader
 import sys
 from sc2reader.events import *
 
+
 class GameEventPlugin():
     # This seems required
     name = "GameEvevntPlugin"
 
     def handleEvent(self, event, replay):
         if (isinstance(event, TrackerEvent)):
-            # print('Tracker Event:\t', event)
+            # print('Tracker Event:\t', event.name, event)
+            if (isinstance(event, UnitDoneEvent)):
+                if (event.unit.owner and event.unit.owner.play_race == "Terran"):
+                    print(f"{event.unit.name} done at {event.second}")
+                
             if (isinstance(event, UnitBornEvent)):
-                print(f'{event.unit.name} ({event.unit_id}) born at: {event.second}')
-                # find closest building
-                if (event.unit.owner):
+                if (event.unit.owner and event.unit.owner.play_race == "Terran"):
+                    print(f'{event.unit.name} ({event.unit_id}) born at: {event.second}')
+                    # find closest building
                     buildings = [b for b in event.unit.owner.units if b.is_building is True and b.finished_at is not None]
-                    print(f'candidate buildings: {len(buildings)}, {", ".join([f"name: {b.name} location: {b.location}" for b in buildings])}')
+                    # print(f'candidate buildings: {len(buildings)}, {", ".join([f"name: {b.name} location: {b.location}" for b in buildings])}')
 
         if (isinstance(event, GameEvent)):
-            print('Game Event:\t', event)
-
-        if (event.name == "BasicCommandEvent"):
-            if (event.ability.is_build is True):
-                print(f"{event.ability.name} at time: {event.second} with build_time: {event.ability.build_time}")
-                # get all completed buildings
-                buildings = [b for b in event.player.units if b.is_building is True and b.finished_at is not None]
-                # if (event.ability.name == "TrainSCV"):
-                    # print(len(buildings))
+            # print('Game Event:\t', event.name, event)
+        
+            if (isinstance(event, BasicCommandEvent)):
+                if (event.ability.is_build is True):
+                    print(f"{event.ability.name} at time: {event.second} with build_time: {event.ability.build_time}")
+                    # get all completed buildings
+                    # buildings = [b for b in event.player.units if b.is_building is True and b.finished_at is not None]
+                    # if (event.ability.name == "TrainSCV"):
+                        # print(len(buildings))
 
 def main():
     replay_file_path = sys.argv[1]
