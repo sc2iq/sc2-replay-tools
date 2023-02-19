66b6e7842be8bf5d9d6598dca27ae2b09ac46dc8
Matt Mazzola <mattmazzola@live.com>
2023-02-19 12:42:53 -0800

    Start work on file upload

--- viewer/app/routes/index.tsx
+++ viewer/app/routes/index.tsx
@@ -1,12 +1,13 @@
-import { DataFunctionArgs, LinksFunction } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
+import { ActionArgs, LinksFunction, LoaderArgs, unstable_composeUploadHandlers, unstable_createMemoryUploadHandler, unstable_parseMultipartFormData } from "@remix-run/node"
+import { Form, useLoaderData } from "@remix-run/react"
 import { buildOrder as badBuildOrder } from "~/buildOrders/bad"
 import { buildOrder as combinedBuildOrder } from "~/buildOrders/combined"
 import { buildOrder as goodBuildOrder } from "~/buildOrders/good"
 import { BuildOrderComponent } from "~/components/buildOrder"
+import uploadFileAsBlobToAzure from "~/services/azureblob.server"
 import indexStyles from "../styles/index.css"
 
-export const loader = async ({ }: DataFunctionArgs) => {
+export const loader = async ({ }: LoaderArgs) => {
   return {
     goodBuildOrder,
     badBuildOrder,
@@ -18,17 +19,46 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: indexStyles },
 ]
 
+export const action = async ({ request }: ActionArgs) => {
+  const uploadHandler = unstable_composeUploadHandlers(
+    async ({ name, contentType, data, filename }) => {
+      console.log({ name, contentType, data, filename })
+      if (name === "replayFileInput"
+        && typeof filename === 'string'
+        && filename.length > 0) {
+        const blobUrl = uploadFileAsBlobToAzure(filename, data)
+
+        return blobUrl
+      }
+
+      return undefined
+    },
+    // parse everything else into memory
+    unstable_createMemoryUploadHandler()
+  )
+
+  const formData = await unstable_parseMultipartFormData(
+    request,
+    uploadHandler // <-- we'll look at this deeper next
+  )
+
+  const formDataEntries = Object.fromEntries(formData)
+  console.log({ formDataEntries })
+
+  return null
+}
+
 export default function Index() {
   const { goodBuildOrder, badBuildOrder, combinedBuildOrder } = useLoaderData<typeof loader>()
 
-
   return (
     <>
-      <div className="uploadForm">
+      <Form method="post" encType="multipart/form-data" className="uploadForm">
         <label htmlFor="replayFileInput">Replay File:</label>
-        <input type="file" id="replayFileInput" name="replayFileInput" />
+        <input type="file" id="replayFileInput" name="replayFileInput" accept=".sc2replay" />
+        <input type="hidden" name="formName" value="replayFile" />
         <button type="submit">Upload</button>
-      </div>
+      </Form>
 
       <h2 className="replayTitle">GoodBuildOrder</h2>
       <BuildOrderComponent buildOrder={goodBuildOrder} />
---
+++ viewer/app/services/azureblob.server.ts
@@ -0,0 +1,21 @@
+import { BlobServiceClient, StorageSharedKeyCredential } from "@azure/storage-blob"
+import invariant from "tiny-invariant"
+
+export default function uploadFileAsBlobToAzure(
+    blobName: string,
+    data: any
+) {
+    invariant(typeof process.env.AZURE_STORAGE_ACCOUNT_NAME === 'string', `You attempt to upload file to blob storage, but the AZURE_STORAGE_ACCOUNT_NAME environment variable was not set!`)
+    invariant(typeof process.env.AZURE_STORAGE_ACCOUNT_KEY === 'string', `You attempt to upload file to blob storage, but the AZURE_STORAGE_ACCOUNT_KEY environment variable was not set!`)
+
+    const sharedKeyCredential = new StorageSharedKeyCredential(
+        process.env.AZURE_STORAGE_ACCOUNT_NAME,
+        process.env.AZURE_STORAGE_ACCOUNT_KEY,
+    )
+    const blobServiceClient = new BlobServiceClient(
+        `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+        sharedKeyCredential
+    )
+    
+    return `${blobServiceClient.accountName} - ${blobName}`
+}
