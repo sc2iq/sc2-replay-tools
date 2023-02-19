0c6cfb0dbd3df8b39b797d19c218ac416eee0155
Matt Mazzola <mattmazzola@live.com>
2023-02-19 13:02:04 -0800

    Add creation on container client to service

--- viewer/app/services/azureblob.server.ts
+++ viewer/app/services/azureblob.server.ts
@@ -7,6 +7,7 @@ export default function uploadFileAsBlobToAzure(
 ) {
     invariant(typeof process.env.AZURE_STORAGE_ACCOUNT_NAME === 'string', `You attempt to upload file to blob storage, but the AZURE_STORAGE_ACCOUNT_NAME environment variable was not set!`)
     invariant(typeof process.env.AZURE_STORAGE_ACCOUNT_KEY === 'string', `You attempt to upload file to blob storage, but the AZURE_STORAGE_ACCOUNT_KEY environment variable was not set!`)
+    invariant(typeof process.env.AZURE_STORAGE_CONTAINER_NAME === 'string', `You attempt to upload file to blob storage, but the AZURE_STORAGE_CONTAINER_NAME environment variable was not set!`)
 
     const sharedKeyCredential = new StorageSharedKeyCredential(
         process.env.AZURE_STORAGE_ACCOUNT_NAME,
@@ -16,6 +17,10 @@ export default function uploadFileAsBlobToAzure(
         `https://${process.env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
         sharedKeyCredential
     )
+
+    const containerClient = blobServiceClient.getContainerClient(process.env.AZURE_STORAGE_CONTAINER_NAME)
+
+    // containerClient.uploadBlockBlob()
     
     return `${blobServiceClient.accountName} - ${blobName}`
 }
